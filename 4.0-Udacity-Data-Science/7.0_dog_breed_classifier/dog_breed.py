ba1664bbda7887f776287430158841e35e4584ef
brentweiliu <brentweiliu@oath.com>
2019-11-24 17:09:18 -0800

    predict breed with Xception is done

--- 4.0-Udacity-Data-Science/7.0_dog_breed_classifier/dog_breed.py
+++ 4.0-Udacity-Data-Science/7.0_dog_breed_classifier/dog_breed.py
@@ -1,49 +1,19 @@
-from sklearn.datasets import load_files
-from keras.utils import np_utils
+
 import numpy as np
 from glob import glob
 from keras.layers import Conv2D, MaxPooling2D, GlobalAveragePooling2D, Input, concatenate
 from keras.layers import Dropout, Flatten, Dense
 from keras.models import Sequential, Model
 
-# define function to load train, test, and validation datasets
-def load_dataset(path):
-    data = load_files(path)
-    dog_files = np.array(data['filenames'])
-    dog_targets = np_utils.to_categorical(np.array(data['target']), 133)
-    return dog_files, dog_targets
-
-# load train, test, and validation datasets
-train_files, train_targets = load_dataset('data/dogImages/train')
-valid_files, valid_targets = load_dataset('data/dogImages/valid')
-test_files, test_targets = load_dataset('data/dogImages/test')
-
 # load list of dog names
 dog_names = [item[20:-1] for item in sorted(glob("data/dogImages/train/*/"))]
-
-# print statistics about the dataset
 print('There are %d total dog categories.' % len(dog_names))
 
 
 import random
 random.seed(8675309)
 
-import cv2
-# extract pre-trained face detector
-face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_alt.xml')
-
-# returns "True" if face is detected in image stored at img_path
-def face_detector(img_path):
-    img = cv2.imread(img_path)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray)
-    return len(faces) > 0
-
-from keras.applications.resnet50 import ResNet50
-# define ResNet50 model
-ResNet50_model = ResNet50(weights='imagenet')
-
-
+# utilities
 from keras.preprocessing import image
 from tqdm import tqdm
 
@@ -59,7 +29,24 @@ def paths_to_tensor(img_paths):
     list_of_tensors = [path_to_tensor(img_path) for img_path in tqdm(img_paths)]
     return np.vstack(list_of_tensors)
 
-from keras.applications.resnet50 import preprocess_input, decode_predictions
+
+# extract pre-trained face detector
+import cv2
+face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_alt.xml')
+
+# returns "True" if face is detected in image stored at img_path
+def face_detector(img_path):
+    img = cv2.imread(img_path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray)
+    return len(faces) > 0
+
+# extract pre-trained isDog detector
+# define ResNet50 model
+from keras.applications.resnet50 import ResNet50
+ResNet50_model = ResNet50(weights='imagenet')
+
+from keras.applications.resnet50 import preprocess_input
 
 def ResNet50_predict_labels(img_path):
     # returns prediction vector for image located at img_path
@@ -72,27 +59,13 @@ def dog_detector(img_path):
     return ((prediction <= 268) & (prediction >= 151))
 
 
+#load pretrain dog breed detector network
 network = 'Xception'
-
-if network =='ResNet-50':
-    bottleneck_features_network = np.load('data/bottleneck_features/DogResnet50Data.npz')
-elif network == 'Inception':
-    bottleneck_features_network = np.load('data/bottleneck_features/DogInceptionV3Data.npz')
-elif network =='Xception':
-    bottleneck_features_network = np.load('data/bottleneck_features/DogXceptionData.npz')
-elif network =='VGG-19':
-    bottleneck_features_network = np.load('data/bottleneck_features/DogVGG19Data.npz')
-
-train_network = bottleneck_features_network['train']
-valid_network = bottleneck_features_network['valid']
-test_network = bottleneck_features_network['test']
-
-print("Shape of train_resnet: {}".format(train_network.shape))
-print("Shape of valid_resnet: {}".format(valid_network.shape))
-print("Shape of test_resnet: {}".format(test_network.shape))
-
+input_shape = (7, 7, 2048)
+print('input_shape', input_shape)
+# define low half network
 Network_model = Sequential()
-Network_model.add(GlobalAveragePooling2D(input_shape=train_network.shape[1:]))
+Network_model.add(GlobalAveragePooling2D(input_shape=input_shape))
 Network_model.add(Dense(133, activation='softmax'))
 Network_model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 
@@ -100,14 +73,7 @@ model_filepath = 'saved_models/weights.best.' + network + '.hd5'
 Network_model.load_weights(model_filepath)
 print("The best weights for the {} model have been loaded".format(network))
 
-# predictions = [np.argmax(Network_model.predict(np.expand_dims(feat, axis=0))) for feat in test_network]
-# # report test accuracy
-# test_accuracy = 100*np.sum(np.array(predictions)==np.argmax(test_targets, axis=1))/len(predictions)
-# print('Test accuracy: %.4f%%' % test_accuracy)
-
-
 from extract_bottleneck_features import *
-
 def network_predict_breed(img_path):
     # extract bottleneck features
     bottleneck_feature = extract_Xception(path_to_tensor(img_path))
@@ -116,8 +82,18 @@ def network_predict_breed(img_path):
     # return dog breed that is predicted by the model
     return dog_names[np.argmax(predicted_vector)]
 
-index = 13
-print("true bread:   ", dog_names[np.argmax(valid_targets[index])])
-print("predict breed:", network_predict_breed(valid_files[index]))
 
-print(network_predict_breed("images/Brittany_02625.jpg"))
+def detect_breed(image_path):
+    print(image_path)
+    if face_detector(image_path):
+        print("Hello, human!")
+    elif dog_detector(image_path):
+        print("Hello, dog!")
+    else:
+        print("Error: Neither a human face or a dog was detected.\n")
+        return
+    print("You look like a ...")
+    print(network_predict_breed(image_path))
+    print()
+
+detect_breed("images/Brittany_02625.jpg")
